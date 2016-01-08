Resolved bug in magnitude calculation

scripts/fastphot.py
@@ -169,7 +169,7 @@ def process(infile, coords, method, inner_radius, outer_radius, cen_method, wind
             phot_data[j]['AREA'] = aperphot_data["area"]
             phot_data[j]['FLUX_ADU'] = aperphot_data["flux"]
             phot_data[j]['FLUX_ELEC'] = phot_data[j]['FLUX_ADU'] * ap.epadu
-            phot_data[j]['MAG'] = ap.zmag - 2.5 * np.log10(phot_data[j]['FLUX_ELEC'])
+            phot_data[j]['MAG'] = ap.zmag - 2.5 * np.log10(phot_data[j]['FLUX_ELEC']/ap.exptime)
             
             # Calculate error in flux - using the formula
             # err = sqrt(flux * gain + npix * (1 + (npix/nsky)) * (flux_sky * gain + R**2))

scripts/photometry.py
@@ -209,7 +209,7 @@ def process(infile, coords, fwhmpsf, sigma, annulus, dannulus):
             phot_data[j]['AREA'] = float(aperphot_data[8])
             phot_data[j]['FLUX_ADU'] = float(aperphot_data[9])
             phot_data[j]['FLUX_ELEC'] = float(aperphot_data[9]) * ap.epadu
-            phot_data[j]['MAG'] = ap.zmag - 2.5 * np.log10(phot_data[j]['FLUX_ELEC'])
+            phot_data[j]['MAG'] = ap.zmag - 2.5 * np.log10(phot_data[j]['FLUX_ELEC']/ap.exptime)
             phot_data[j]['MERR'] = float(aperphot_data[10])
             phot_data[j]['PIER'] = int(aperphot_data[11])
             
3aaa5b41ba5394fdbd60fefa0a662c49c71be25e
Lee J. Rosenthal <leerosenthalj@gmail.com>
2017-07-31 13:48:05 -0700

    Fixed bug in aperphot curve-of-growth function.

--- chimera/aperphot.py
+++ chimera/aperphot.py
@@ -6,7 +6,7 @@ import numpy as np
 from pyraf import iraf
 from datetime import datetime, timedelta
 
-    
+
 MONTHS = {"Jan": 1, "Feb": 2, "March": 3, "April": 4, "May": 5, "June": 6, "July": 7, "Aug": 8, "Sept": 9, "Oct": 10, "Nov": 11, "Dec": 12}
 
 
@@ -14,14 +14,14 @@ class Aperphot:
     def __init__(self, sci_file, coords):
         self.sci_file = sci_file
         self.coords = coords
-        
+
         # load configuration file
         cfg = config.Config()
         self.cfg_data = cfg.load()
-        
+
         # Set header keyword parameters
         self.setkeywords()
-        
+
         # Set parameters
         self.setparams()
 
@@ -29,10 +29,10 @@ class Aperphot:
     def setkeywords(self):
         """
         Set FITS image header keyword parameters.
-        
+
         Parameters
         ----------
-        
+
         Returns
         -------
         None
@@ -50,22 +50,22 @@ class Aperphot:
         self.preampg = header["PREAMPG"].strip()
         utcstart = header["UTCSTART"]
         self.utcstart = self.parser(utcstart)
-        
+
         return
-        
-        
+
+
     def setparams(self):
         """
         Set datapars, centerpars, fitskypars and photpars.
-        
+
         Parameteres
         -----------
-        
+
         Returns
         -------
         None
-        """  
-        # Set parameters for daophot 
+        """
+        # Set parameters for daophot
         self.fwhmpsf = self.cfg_data["Phot"]["fwhmpsf"]
         self.sigma = self.cfg_data["Phot"]["sigma"]
         self.exposure = self.cfg_data["Phot"]["exposure"]
@@ -74,8 +74,8 @@ class Aperphot:
         self.cbox = self.cfg_data["Phot"]["cbox"]
         self.maxshift = self.cfg_data["Phot"]["maxshift"]
 
-        self.salgorithm = self.cfg_data["Phot"]["salgorithm"] 
-        self.annulus = self.cfg_data["Phot"]["annulus"] 
+        self.salgorithm = self.cfg_data["Phot"]["salgorithm"]
+        self.annulus = self.cfg_data["Phot"]["annulus"]
         self.dannulus = self.cfg_data["Phot"]["dannulus"]
 
         self.apertures = self.cfg_data["Phot"]["apertures"]
@@ -87,47 +87,47 @@ class Aperphot:
         if self.amptype == "EMGAIN":
             self.readnoise /= self.emgain
             self.epadu /= self.emgain
-                                
+
         # Set parameters for phot
         self.method = "exact"
         self.inner_radius = 14
         self.outer_radius = 30
 
         return
-        
-        
+
+
     def setiraf(self):
         """
         Set IRAF global parameters and load DAOPHOT package for aperture
         photometry.
-        
+
         Parameters
         ----------
-        
+
         Returns
         -------
         None
         """
         iraf.prcacheOff()
         iraf.set(writepars=0)
-    
+
         # Load IRAF packages
         iraf.noao(_doprint = 0)
         iraf.noao.digiphot(_doprint = 0)
         iraf.noao.digiphot.daophot(_doprint = 0)
-    
+
         return
-        
-                        
+
+
     def parser(self, utcstart):
         """
         Datetime parser for CHIMERA UTCSTART header keyword.
-        
+
         Parameters
         ----------
         utcstart : string
             Datetime for start of frame (in UTC)
-            
+
         Returns
         -------
         dt : datetime struct
@@ -137,46 +137,46 @@ class Aperphot:
         month = MONTHS[month]
         date = int(date)
         year = int(year)
-        
+
         hour, minu, sec = time.split(":")
         hour = int(hour)
         minu = int(minu)
         sec, ms = sec.split(".")
         sec = int(sec)
         ms = int(ms) * 1000
-        
+
         dt = datetime(year, month, date, hour, minu, sec, ms)
-        
+
         return dt
-    
+
 
     def addtime(self, secs):
         """
         Add time in seconds to UTC datetime.
-        
+
         Parameters
         ----------
         secs : float
             Time to add to UTC in seconds.
-            
+
         Returns
         -------
         dt : datetime structure
-        """    
+        """
         td = timedelta(0, secs)
 
         return self.utcstart + td
-        
-            
+
+
     def daocog(self, tolerance = 0.01):
         """
         Curve of growth to determine nominal aperture for photometry using DAOPHOT.
-        
+
         Parameters
         ----------
         tolerance : float
             Magnitude difference tolerance between different apertures
-        
+
         Returns
         -------
         aperture : float
@@ -184,12 +184,12 @@ class Aperphot:
         """
         # load iraf packages
         self.setiraf()
-        
+
         # Randomly peform curve of growth on 5 frames
         framenum = np.random.randint(1, self.nframes, 5)
-        
+
         apertures = np.linspace(2,20,19)
-        
+
         # Iterate through the frames and determine nominal aperture
         nom_aper = np.zeros(5, dtype = np.float32)
         cnt = 0
@@ -208,20 +208,20 @@ class Aperphot:
                 nom_aper[cnt] = 10.0
             cnt += 1
             iraf.delete(outfile)
-            
+
         return np.median(nom_aper)
-        
+
 
     def cog(self, window_size, method, tolerance = 0.01):
         """
-        Curve of growth to determine nominal aperture for photometry using 
+        Curve of growth to determine nominal aperture for photometry using
         astropy photutils.
-        
+
         Parameters
         ----------
         tolerance : float
             Magnitude difference tolerance between different apertures
-        
+
         Returns
         -------
         aperture : float
@@ -230,16 +230,16 @@ class Aperphot:
         # Aperture values in pixels
         apertures = np.array([2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
         naper = apertures.shape[0]
-        
+
         # Randomly perform curve of growth on 5 frames
         framenum = np.random.randint(1, self.nframes, 5)
-        
+
         apertures = np.linspace(2,20,19)
-        
+
         # Read input image and star position
         image = chimera.fitsread(self.sci_file)
         pos = np.loadtxt(self.coords, ndmin = 2)
-        
+
         # Iterate through the frames and determine nominal aperture
         nom_aper = np.zeros(5, dtype = np.float32)
         cnt = 0
@@ -248,7 +248,7 @@ class Aperphot:
             objpos = chimera.recenter(image[val,:,:], pos, window_size, method)
             for i in range(naper):
                 flux = self.phot(image[val,:,:], objpos, aper = apertures[i])
-                mags_arr[i] = -2.5 * np.log10(flux['flux'])
+                mags_arr[i] = -2.5 * np.log10(flux['flux'][i])
             mags_diff = np.diff(mags_arr)
             idx = np.where((np.abs(mags_diff) < 0.01) & (np.abs(mags_diff) != 0.0))
             if len(idx[0]) != 0:
@@ -256,78 +256,78 @@ class Aperphot:
             else:
                 nom_aper[cnt] = 10.0
             cnt += 1
-            
+
         return np.median(nom_aper)
-        
-                
+
+
     def daophot(self, framenum, coords, outfile, apertures, verbose = "no"):
         """
         Aperture photometry of stars in the coords file using IRAF PHOT routine.
-        
+
         Parameters
         ----------
         framenum : int
             Frame number in the image cube to perform aperture photometry on.
-            
+
         coords : string
             Text file with coordinate of the stars
-            
+
         outfile : string
             Text file to which photometry results are written
-        
+
         Returns
         -------
         None
         """
         # load iraf packages
         self.setiraf()
-        
+
         iraf.delete(outfile)
-        iraf.phot(image = self.sci_file + "[,," + str(framenum) + "]", coords = coords, output = outfile, fwhmpsf = self.fwhmpsf, sigma = self.sigma, readnoise = self.readnoise, epadu = self.epadu, exposure = self.exposure, calgorithm = self.calgorithm, cbox = self.cbox, maxshift = self.maxshift, salgorithm = self.salgorithm, annulus = self.annulus, dannulus = self.dannulus, apertures = apertures, zmag = self.zmag, interactive = "no", verify = "no", verbose = verbose)        
-        
+        iraf.phot(image = self.sci_file + "[,," + str(framenum) + "]", coords = coords, output = outfile, fwhmpsf = self.fwhmpsf, sigma = self.sigma, readnoise = self.readnoise, epadu = self.epadu, exposure = self.exposure, calgorithm = self.calgorithm, cbox = self.cbox, maxshift = self.maxshift, salgorithm = self.salgorithm, annulus = self.annulus, dannulus = self.dannulus, apertures = apertures, zmag = self.zmag, interactive = "no", verify = "no", verbose = verbose)
+
         return
-        
-        
+
+
     def phot(self, image, objpos, aper):
         """
         Aperture photometry using Astropy's photutils.
-        
+
         Parameters
         ----------
         image : numpy array
             2D image array
-            
+
         objpos : list of tuple
             Object poistions as list of tuples
-            
+
         aper : float
             Aperture radius in pixels
-         
-        Returns 
+
+        Returns
         -------
         phot_table : astropy table
-             Output table with stellar photometry   
+             Output table with stellar photometry
         """
         try:
             from astropy.table import hstack
             from photutils import aperture_photometry, CircularAnnulus, CircularAperture
         except ImportError:
             pass
-    
-        apertures = CircularAperture(objpos, r = aper) 
+
+        apertures = CircularAperture(objpos, r = aper)
         annulus_apertures = CircularAnnulus(objpos, r_in = self.inner_radius, r_out = self.outer_radius)
-        
+
         rawflux_table = aperture_photometry(image, apertures = apertures, method = self.method)
         bkgflux_table = aperture_photometry(image, apertures = annulus_apertures, method = self.method)
         phot_table = hstack([rawflux_table, bkgflux_table], table_names = ["raw", "bkg"])
-        
+
         bkg = phot_table["aperture_sum_bkg"] / annulus_apertures.area()
         phot_table["msky"] = bkg
         phot_table["area"] = apertures.area()
         phot_table["nsky"] = annulus_apertures.area()
-                
+
         bkg_sum = bkg * apertures.area()
         final_sum = phot_table["aperture_sum_raw"] - bkg_sum
         phot_table["flux"] = final_sum
-        
+
         return phot_table
